621a713d400e7bafcab9b9075ac418f0da708d89
raghav0902188 <raghav.sharma@aol.com>
2021-10-31 00:36:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value.toFixed(2);
+    var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
